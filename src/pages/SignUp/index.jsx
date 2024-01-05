e9a9b368c4a629121dec3c4fe4bbfb902a8c8f26
vitor-henridias <vhdias.dev@gmail.com>
2024-01-04 21:47:44 -0300

    wip

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,42 +1,77 @@
 import { FiLock, FiMail, FiUser } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Background, Container, Form } from "./styles";
+import { useState } from "react";
+import { api } from "../../services/api";
 
 export function SignUp() {
-    return (
-        <Container>
-            <Background />
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-            <Form>
-                <h1>Rocket Notes</h1>
-                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+  const navigate = useNavigate();
 
-                <h2>Crie sua conta</h2>
+  function handleSingUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
 
-                <Input
-                    placeholder="Nome"
-                    type="text"
-                    icon={FiUser}
-                />
+    api
+      .post("/users", {
+        name,
+        email,
+        password,
+      })
+      .then(function (response) {
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
+      })
+      .catch(function (error) {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possivel cadastrar!");
+        }
+      });
+  }
 
-                <Input
-                    placeholder="E-mail"
-                    type="text"
-                    icon={FiMail}
-                />
+  return (
+    <Container>
+      <Background />
 
-                <Input
-                    placeholder="Senha"
-                    type="password"
-                    icon={FiLock}
-                />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-                <Button title="Cadastrar" />
+        <h2>Crie sua conta</h2>
 
-                <Link to="/">Voltar para o login</Link>
-            </Form>
-        </Container>
-    );
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button title="Cadastrar" onClick={handleSingUp} />
+
+        <Link to="/">Voltar para o login</Link>
+      </Form>
+    </Container>
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,8 +6,8 @@ import { AuthRoutes } from "./auth.routes";
 export function Routes() {
     return (
         <BrowserRouter>
-            {/* <AuthRoutes /> */}
-            <AppRoutes />
+            <AuthRoutes />
+            {/* <AppRoutes /> */}
         </BrowserRouter>
     );
 }
